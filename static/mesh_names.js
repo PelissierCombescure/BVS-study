e814d6b3a0fbc82b9d82fa2eeebdeeda9c3cc60b
mpelissi <marie.pelissiercombescure@toulouse-inp.fr>
2023-04-04 10:53:41 +0200

    obj names tous ok sauf ashtray bizarre avec le trou

--- static/mesh_names.js
+++ static/mesh_names.js
@@ -1,3 +1,57 @@
+obj_names = ['octopus_update_normed_centered_user_study.obj',
+'casting_update_normed_centered_user_study.obj',
+'carVasa_update_normed_centered_user_study.obj',
+'blade_update_normed_centered_user_study.obj',
+'bunny_update_normed_centered_user_study.obj',
+'igea_update_normed_centered_user_study.obj',
+'camel_update_normed_centered_user_study.obj',
+'Harley_update_normed_centered_user_study.obj',
+'MaxPlanck_update_normed_centered_user_study.obj',
+'gorgoile_update_normed_centered_user_study.obj',
+'prot_update_normed_centered_user_study.obj',
+'michael3_update_normed_centered_user_study.obj',
+'A380_update_normed_centered_user_study.obj',
+'chair107_update_normed_centered_user_study.obj',
+'dragon_update_normed_centered_user_study.obj',
+'rockerArm_update_normed_centered_user_study.obj',
+'michael8_update_normed_centered_user_study.obj',
+'vase_update_normed_centered_user_study.obj',
+'fandisk_update_normed_centered_user_study.obj',
+'james_update_normed_centered_user_study.obj',
+'cow_update_normed_centered_user_study.obj',
+'dinosaur_update_normed_centered_user_study.obj',
+'flowerpot_update_normed_centered_user_study.obj',
+'bimba_update_normed_centered_user_study.obj',
+'horse_update_normed_centered_user_study.obj',
+'turbine_update_normed_centered_user_study.obj',
+'basket_regulier_tri_normed_centered_user_study.obj',
+'calculator_regulier_tri_normed_centered_user_study.obj',
+'eyeglasses_regulier_tri_normed_centered_user_study.obj',
+'cap_regulier_tri_normed_centered_user_study.obj',
+'camera_regulier_tri_normed_centered_user_study.obj',
+'bus_regulier_tri_normed_centered_user_study.obj',
+'cup_regulier_tri_normed_centered_user_study.obj',
+'backpack_regulier_tri_normed_centered_user_study.obj',
+'comb_regulier_tri_normed_centered_user_study.obj',
+'fire_extinguisher_regulier_tri_normed_centered_user_study.obj',
+'bicycle_regulier_tri_normed_centered_user_study.obj',
+'coffee_regulier_tri_normed_centered_user_study.obj',
+'boat_regulier_tri_normed_centered_user_study.obj',
+'ashtray_regulier_tri_normed_centered_user_study.obj',
+'eraser_regulier_tri_normed_centered_user_study.obj',
+'bucket_regulier_tri_normed_centered_user_study.obj',
+'bench_regulier_tri_normed_centered_user_study.obj',
+'cabinet_regulier_tri_normed_centered_user_study.obj']
+
+
+
+
+
+
+
+
+
+
 // obj_names =  [ 'octopus_update_normed_centered_user_study.obj',
 //  'casting_update_normed_centered_user_study.obj',
 //  'carVasa_update_normed_centered_user_study.obj',
@@ -23,12 +77,10 @@
 //  'flowerpot_update_normed_centered_user_study.obj',
 //  'bimba_update_normed_centered_user_study.obj',
 //  'horse_update_normed_centered_user_study.obj',
-//  'turbine_update_normed_centered_user_study.obj',
-//   // objecnet3d
-//  'backpack_regulier_tri_centered_user_study_normed.obj']
+//  'turbine_update_normed_centered_user_study.obj']
 
 
- obj_names =  ['basket_regulier_tri_normed_centered_user_study.obj',
+ //obj_names =  [//'basket_regulier_tri_normed_centered_user_study.obj',
  //'calculator_regulier_tri_normed_centered_user_study.obj',
  //'eyeglasses_regulier_tri_normed_centered_user_study.obj',
  //'cap_regulier_tri_normed_centered_user_study.obj',
@@ -44,6 +96,5 @@
  //'ashtray_regulier_tri_normed_centered_user_study.obj',
  //'eraser_regulier_tri_normed_centered_user_study.obj',
  //'bucket_regulier_tri_normed_centered_user_study.obj',
- 'bench_regulier_tri_normed_centered_user_study.obj',
- //'cabinet_regulier_tri_normed_centered_user_study.obj'
-]
+ //'bench_regulier_tri_normed_centered_user_study.obj',
+ //'cabinet_regulier_tri_normed_centered_user_study.obj']
