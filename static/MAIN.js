e259c91f176190d5f8028dbad847edbbb5d1352d
mpelissi <marie.pelissiercombescure@toulouse-inp.fr>
2023-04-25 12:02:32 +0200

    ras

--- static/MAIN.js
+++ static/MAIN.js
@@ -47,10 +47,10 @@ function init_variable(premier_appel){
     obj_file_random = shuffle(obj_names)
 
     // nombre de mesh a visionner AU TOTAL
-    nb_mesh = 3//obj_file_random.length
+    nb_mesh = 10//obj_file_random.length
 
     // nb analyse demandé
-    nb_analyse_demande = 2
+    nb_analyse_demande = 5
     list_idx_tache =[]
     for (let p=0; p<nb_mesh; p++){list_idx_tache.push(p+1)}
     list_idx_tache = shuffle(list_idx_tache)
